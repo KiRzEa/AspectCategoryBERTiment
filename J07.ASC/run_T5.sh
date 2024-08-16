3d8efcf7935bc239ebdbc0716f98452c08d1a1d5
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-08-16 11:00:33 +0700

    modify run_T5.sh

--- J07.ASC/run_T5.sh
+++ J07.ASC/run_T5.sh
@@ -11,7 +11,7 @@ dataset_types=("Hotel" "Phone")
 for model_id in "${model_ids[@]}"; do
   for domain in "${dataset_types[@]}"; do
     # Construct the Python script command with current model ID and dataset type
-    python script_T5.py --model_id "$model_id" --domain "$domain"  --lr 3e-4 --num_epochs 20 --batch_size 8 --seed 42
+    python script_T5.py --model_id "$model_id" --domain "$domain"  --lr 3e-4 --num_epochs 20 --batch_size 4 --seed 42
     
   done
 done
